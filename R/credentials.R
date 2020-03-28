26b415be6bd08e979d344b48244626211203786e
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-03-28 17:53:06 +0000

    Allow credentials in non-interactive sessions

--- R/credentials.R
+++ R/credentials.R
@@ -10,15 +10,16 @@ get_credentials <- function() {
   list(
     username = get_single_credential("SHAREPOINT_USERNAME",
                                      "Sharepoint username",
-                                     read_line),
-    password = get_single_credential("SHAREPOINT_PASS", "Sharepoint password",
+                                     get_string),
+    password = get_single_credential("SHAREPOINT_PASS",
+                                     "Sharepoint password",
                                      get_pass)
   )
 }
 
 get_single_credential <- function(env_var, credential, read_func) {
   cred <- Sys.getenv(env_var)
-  if (is_empty(cred) && is_interactive()) {
+  if (is_empty(cred)) {
     cred <- read_func(paste0(credential, ": "))
   }
   tryCatch(
@@ -26,14 +27,9 @@ get_single_credential <- function(env_var, credential, read_func) {
     error = function(e) {
       e$message <- paste0(
         e$message,
-        sprintf(", either set env var %s or enter in interactive session",
+        sprintf(", either set env var %s or enter at prompt",
                 env_var))
       stop(e)
     })
   cred
-}
-
-## This exists just so we can mock it in tests as you can't mock base functions
-is_interactive <- function() {
-  interactive() # nocov
 }
--- R/utils.R
+++ R/utils.R
@@ -6,8 +6,9 @@ get_pass <- function(prompt) {
   getPass::getPass(prompt, TRUE) # nocov
 }
 
-read_line <- function(prompt) {
-  clean_input_text(readline(prompt = prompt)) # nocov
+get_string <- function(prompt) {
+  message(prompt, appendLF = FALSE)
+  clean_input_text(scan("stdin", character(), n = 1, quiet = TRUE))
 }
 
 clean_input_text <- function(x) {
