14bdfc6fc9165a44455294c92aee304470dbca98
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-26 17:42:14 +0100

    Start testing folder interface

--- tests/testthat/mocks/README.md
+++ tests/testthat/mocks/README.md
@@ -7,3 +7,10 @@ This is because we only have access to 1 sharepoint instance and we only have pe
 Mocks:
 * security_token_response.rds - The response from `httr::POST` call sent during `get_security_token` with several unneeded fields removed (e.g. cookies, headers etc.) and the content updated manually so the token is a safe one we can use for testing `t=EXAMPLE_TOKEN==&amp;p=`
 * cookies_response.rds - The response from `httr:POST` call sent during `get_access_cookies` with unneeded fields removed and cookies updated manually to be a save one we can use for testing. 
+
+### Creating a new mock response
+
+* insert a browser() call just before the call to a client method
+* save the response to `mocks/<action>_response_raw.rds`
+* strip the url, headers and cookies back to desensitise it (strip_response)
+* modify the content as needed
---
+++ tests/testthat/test-sharepoint-folder.R
@@ -0,0 +1,103 @@
+context("sharepoint_folder")
+
+test_that("list files", {
+  p <- mock_pointr()
+  folder <- p$folder("site", "path")
+  expect_is(folder, "sharepoint_folder")
+
+  folder_files_res <- readRDS("mocks/folder_files_response.rds")
+  mock_get <- mockery::mock(folder_files_res)
+  dat <- with_mock("httr::GET" = mock_get,
+                   folder$files())
+  expect_is(dat, "tbl_df")
+  expect_equal(names(dat), c("name", "size", "created", "modified"))
+  expect_equal(dat$name, c("clipboard.txt", "test.txt"))
+})
+
+
+test_that("list folders", {
+  p <- mock_pointr()
+  folder <- p$folder("site", "path")
+  expect_is(folder, "sharepoint_folder")
+
+  folder_folder_res <- readRDS("mocks/folder_folders_response.rds")
+  mock_get <- mockery::mock(folder_folder_res)
+  dat <- with_mock("httr::GET" = mock_get,
+                   folder$folders())
+  expect_is(dat, "tbl_df")
+  expect_equal(names(dat), c("name", "items", "created", "modified"))
+  expect_equal(dat$name, c("data_offers", "administration_station"))
+})
+
+
+test_that("list everything", {
+  p <- mock_pointr()
+  folder <- p$folder("site", "path")
+  expect_is(folder, "sharepoint_folder")
+
+  folder_files_res <- readRDS("mocks/folder_files_response.rds")
+  folder_folders_res <- readRDS("mocks/folder_folders_response.rds")
+  mock_get <- mockery::mock(folder_folders_res, folder_files_res)
+
+  dat <- with_mock("httr::GET" = mock_get, folder$list())
+  expect_is(dat, "tbl_df")
+  expect_equal(names(dat),
+               c("name", "items", "size", "is_folder", "created", "modified"))
+  expect_equal(dat$name, c("data_offers", "administration_station",
+                           "clipboard.txt", "test.txt"))
+})
+
+
+test_that("get parent directory", {
+  p <- mock_pointr()
+  folder <- p$folder("site", "a/b/c")
+  parent <- folder$parent()
+  expect_equal(r6_private(parent)$path, "a/b")
+  expect_equal(r6_private(parent$parent()$parent())$path, ".")
+})
+
+
+test_that("download from folder", {
+  p <- mock_pointr()
+  folder <- p$folder("site", "a/b/c")
+
+  mock_get <- mockery::mock(mock_response())
+  res <- with_mock(
+    "httr::GET" = mock_get,
+    folder$download("file.txt"))
+
+  expect_match(res, "\\.txt$")
+  mockery::expect_called(mock_get, 1)
+  expect_equal(
+    mockery::mock_args(mock_get)[[1]][[1]],
+    paste0("https://httpbin.org//sites/site/_api/web/",
+           "GetFolderByServerRelativeURL('a/b/c')/Files('file.txt')/$value"))
+})
+
+
+test_that("upload", {
+  p <- mock_pointr()
+  folder <- p$folder("site", "a/b/c")
+
+  contextinfo_res <- readRDS("mocks/contextinfo_response.rds")
+  upload_res <- structure(list(status_code = 200), class = "response")
+  mock_post <- mockery::mock(contextinfo_res, upload_res)
+  tmp <- tempfile()
+  writeLines("content", tmp)
+
+  res <- with_mock(
+    "httr::POST" = mock_post,
+    folder$upload(tmp, "file.txt"))
+  expect_null(res)
+
+  mockery::expect_called(mock_post, 2)
+  expect_equal(
+    mockery::mock_args(mock_post)[[1]][[1]],
+    "https://httpbin.org//sites/site/_api/contextinfo")
+
+  expect_equal(
+    mockery::mock_args(mock_post)[[2]][[1]],
+    paste0("https://httpbin.org//sites/site/_api/web/",
+           "GetFolderByServerRelativeURL('a/b/c')/Files/",
+           "Add(url='file.txt',overwrite=true)"))
+})
